22831c58c5defacb933f76c2df7c823d5f6b4531
Behzod <behzodtuit@gmail.com>
2023-12-23 10:07:54 +0500

    deploy

--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--7heed60_o=ujo)!id%l8v&0ell49m^2vt0%ur=b0wk@9$93du'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
