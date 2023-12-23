e82efe86d20d7a2d9e1772a9d6dc5356140009d2
Behzod <behzodtuit@gmail.com>
2023-12-23 11:02:35 +0500

    deploy

--- project/settings.py
+++ project/settings.py
@@ -127,4 +127,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CORS_ALLOW_ALL_ORIGINS=True
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5500",
+]
